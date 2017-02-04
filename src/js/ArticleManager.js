b715cc111dc6a7f0392a5e649c17c528930dda87
Pedro Bala Simón <pbalasimon@gmail.com>
2017-02-05 00:48:10 +0100

    Marcar likes articulos al cargar la página

--- src/js/ArticleManager.js
+++ src/js/ArticleManager.js
@@ -12,5 +12,15 @@ module.exports = {
             ArticleService.like(id);
             $article.find("i").removeClass("fa-heart-o").addClass("fa-heart");
         }
+    },
+    setLikes: function () {
+        var articles = $("#articles").find("article");
+        articles.each(function () {
+            var id = $(this).parents().find("article").data("id");
+            var liked = ArticleService.isLiked(id);
+            if (liked) {
+                $(this).find("i").removeClass("fa-heart-o").addClass("fa-heart");
+            }
+        })
     }
 }
--- src/js/ready.js
+++ src/js/ready.js
@@ -2,10 +2,12 @@ var ArticleManager = require('./ArticleManager');
 
 $(document).ready(function () {
 
+    // marcar like articulos
+    ArticleManager.setLikes();
+
+
     $("#like").on("click", function () {
         ArticleManager.like(this);
     });
-    // cargar las canciones
-    //SongsListManager.loadSongs();
 
 });
