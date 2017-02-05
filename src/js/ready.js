ef6f4aa60b4d7584aa7ba0820accf5c241edae81
Pedro Bala Simón <pbalasimon@gmail.com>
2017-02-05 16:13:44 +0100

    Formulario comentarios y comentairos validado formulario con jquery validation

--- src/js/ready.js
+++ src/js/ready.js
@@ -1,4 +1,5 @@
 var ArticleManager = require('./ArticleManager');
+require('jquery-validation');
 
 $(document).ready(function () {
 
@@ -30,6 +31,50 @@ $(document).ready(function () {
     $(".like").on("click", function () {
         var id = $(this).parent().closest('article').data("id");
         ArticleManager.like(id);
+    });
+
+    jQuery.validator.addMethod("validateWords", function (value, element) {
+        var phraseReplaceSpaces = value.replace(/\s\s+/g, ' ').trim();
+        var arrayOfWords = phraseReplaceSpaces.split(' ');
+        var numberOfWords = arrayOfWords.length;
+        return numberOfWords < 120;
+    }, "El número máximo de palabras permitido es 120");
+
+    $("#add-comment-form").validate({
+        debug: true,
+        errorClass: "has-danger",
+        rules: {
+            name: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            comment: {
+                validateWords: true,
+                required: true
+            }
+        },
+        messages: {
+            name: "Por favor, introduzca un nombre",
+            email: {
+                required: "Por favor, introduzca una dirección de email",
+                email: "La dirección de email debe tener el formato nombre@dominio.es"
+            },
+            comment: {
+                required: "Por favor, introduzca un comentario",
+            }
+
+        },
+        submitHandler: function (form) {
+            console.log("Formulario validado");
+            return false;
+        },
+        highlight: function (element, errorClass) {
+            $(element).parent().closest("div.form-group").addClass(errorClass);
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            $(element).parent().closest(".has-danger").removeClass(errorClass).addClass("has-success");
+        }
     });
 
 });
